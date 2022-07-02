d8ffc78a90f2931f2b7313bae35640425a6fba81
Oreol Noumodong <oreolnoumodong@gmail.com>
2022-07-02 07:30:37 +0100

    Add Head to testing pages

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -12,7 +12,7 @@ function Profile(props: any) {
       <Head>
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>404 -Page Not Found</title>
+        <title>Profile</title>
       </Head>
 
       <div className="mt-8">
