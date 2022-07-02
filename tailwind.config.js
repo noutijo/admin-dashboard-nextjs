4a5f96aab579607aa19754f8ab3d476775bad2aa
Oreol Noumodong <oreolnoumodong@gmail.com>
2022-07-02 01:55:26 +0100

    extend colors from tailwind file config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,27 +5,6 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
-    colors: {
-      "colorPrimary": "#7380ec",
-      "colorDanger": "#ff7782",
-      "colorSuccess": "#41f1b6",
-      "colorWarning": "#ffbb55",
-      "colorWhite": "#fff",
-      "colorInfoDark": "#7d8da1",
-      "colorInfoLight": "#dce1eb",
-      "colorDark": "#363949",
-      "colorLight": "rgba(132, 139, 200, 0.18)",
-      "colorPrimaryVariant": "#111e88",
-      "colorDarkVariant": "#677483",
-      "colorBackground": "#f6f6f9",
-      "colorTransparent": "transparent",
-      //dark variant colors
-      "colorBackground_DM": "#181a1e",
-      "colorWhite_DM": "#202528",
-      "colorDark_DM": "#edeffd",
-      "colorDarkVariant_DM": "#a3bdCC",
-      "colorLight_DM": "rgba(0, 0, 0, 0.4)",
-    },
     screens: {
       'sm': '0px',
       'md': '1024px',
@@ -34,6 +13,27 @@ module.exports = {
       '2xl': '1920px',
     },
     extend: {
+      colors: {
+        "colorPrimary": "#7380ec",
+        "colorDanger": "#ff7782",
+        "colorSuccess": "#41f1b6",
+        "colorWarning": "#ffbb55",
+        "colorWhite": "#fff",
+        "colorInfoDark": "#7d8da1",
+        "colorInfoLight": "#dce1eb",
+        "colorDark": "#363949",
+        "colorLight": "rgba(132, 139, 200, 0.18)",
+        "colorPrimaryVariant": "#111e88",
+        "colorDarkVariant": "#677483",
+        "colorBackground": "#f6f6f9",
+        "colorTransparent": "transparent",
+        //dark variant colors
+        "colorBackground_DM": "#181a1e",
+        "colorWhite_DM": "#202528",
+        "colorDark_DM": "#edeffd",
+        "colorDarkVariant_DM": "#a3bdCC",
+        "colorLight_DM": "rgba(0, 0, 0, 0.4)",
+      },
       keyframes: {
         smmenu: {
           'from': { left: '-100%' },
