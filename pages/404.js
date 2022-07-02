5c9dfd760efb5741f36a00411f3123545931cd77
Oreol Noumodong <oreolnoumodong@gmail.com>
2022-07-02 07:34:00 +0100

    Add Head to testing pages

--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 const Home = () => {
     return (
@@ -20,6 +21,7 @@ const Home = () => {
                     <p className="mt-2 text-sm text-center">
                         <a className="flex items-center justify-center gap-1 text-colorPrimary" href="https://oreolnoumodong.com" target="_blank" rel="noopener noreferrer"><span className="material-icons-sharp flex items-center justify-center">link</span> www.oreolnoumodong.com</a>
                     </p>
+                    <Link href="/">Go to Home</Link>
                 </div>
 
             </div>
