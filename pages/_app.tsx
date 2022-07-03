1c8f5a186aa09e910fedec7bc0e442019a7d9c21
Oreol Noumodong <oreolnoumodong@gmail.com>
2022-07-03 19:06:36 +0100

    Use default import path for global style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import '../styles/globals.css';
+import 'styles/globals.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import type { AppProps } from 'next/app';
-import { wrapper } from 'app/store';
-import Layout from 'components/layout/Layout';
+import { wrapper } from '@app/store';
+import Layout from '@components/layout/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return  (
