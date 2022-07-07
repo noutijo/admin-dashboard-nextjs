7f9ded8be14d295714943c2bf9d3fc7769df4cac
Oreol Noumodong <oreolnoumodong@gmail.com>
2022-07-07 17:30:09 +0100

    Add my own scale & rotate utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,48 +7,48 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
     "./data/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
     screens: {
-      'sm': '320px',
-      'md': '1024px',
-      'lg': '1280px',
-      'xl': '1536px',
-      '2xl': '1920px',
+      sm: "320px",
+      md: "1024px",
+      lg: "1280px",
+      xl: "1536px",
+      "2xl": "1920px",
     },
     extend: {
       fontFamily: {
         sans: ["Poppins", "Karla", ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        "colorPrimary": "#7380ec",
-        "colorDanger": "#ff7782",
-        "colorSuccess": "#41f1b6",
-        "colorWarning": "#ffbb55",
-        "colorWhite": "#fff",
-        "colorInfoDark": "#7d8da1",
-        "colorInfoLight": "#dce1eb",
-        "colorDark": "#363949",
-        "colorLight": "rgba(132, 139, 200, 0.18)",
-        "colorPrimaryVariant": "#111e88",
-        "colorDarkVariant": "#677483",
-        "colorBackground": "#f6f6f9",
-        "colorTransparent": "transparent",
+        colorPrimary: "#7380ec",
+        colorDanger: "#ff7782",
+        colorSuccess: "#41f1b6",
+        colorWarning: "#ffbb55",
+        colorWhite: "#fff",
+        colorInfoDark: "#7d8da1",
+        colorInfoLight: "#dce1eb",
+        colorDark: "#363949",
+        colorLight: "rgba(132, 139, 200, 0.18)",
+        colorPrimaryVariant: "#111e88",
+        colorDarkVariant: "#677483",
+        colorBackground: "#f6f6f9",
+        colorTransparent: "transparent",
         //dark variant colors
-        "colorBackground_DM": "#181a1e",
-        "colorWhite_DM": "#202528",
-        "colorDark_DM": "#edeffd",
-        "colorDarkVariant_DM": "#a3bdCC",
-        "colorLight_DM": "rgba(0, 0, 0, 0.4)",
+        colorBackground_DM: "#181a1e",
+        colorWhite_DM: "#202528",
+        colorDark_DM: "#edeffd",
+        colorDarkVariant_DM: "#a3bdCC",
+        colorLight_DM: "rgba(0, 0, 0, 0.4)",
       },
       keyframes: ({ theme }) => ({
         showmenu: {
-          'from': { left: '-100%' },
-          'to': { left: '0%' },
+          from: { left: "-100%" },
+          to: { left: "0%" },
         },
         hidemenu: {
-          'from': { left: '0%' },
-          'to': { left: '-100%' },
+          from: { left: "0%" },
+          to: { left: "-100%" },
         },
         flashlight: {
           "0%": {
@@ -62,19 +62,29 @@ module.exports = {
           "100%": {
             background: theme("colors.colorInfoLight / 0%"),
           },
-        }
+        },
       }),
       animation: {
-        'showmenu': 'showmenu 400ms ease forwards',
-        'hidemenu': 'hidemenu 400ms ease forwards',
-        'flashlight': 'flashlight 2s ease-in-out 1',
-      }
-    }
+        showmenu: "showmenu 400ms ease forwards",
+        hidemenu: "hidemenu 400ms ease forwards",
+        flashlight: "flashlight 2s ease-in-out 1",
+      },
+    },
   },
   plugins: [
-    require('@tailwindcss/line-clamp'),
+    require("@tailwindcss/line-clamp"),
     plugin(({ addUtilities }) => {
-
-    })
+      const newUtilities = {
+        ".scale-1": {
+          transform: "scale(1)",
+        },
+        ".rotate-1": {
+          transform: "rotate(1)",
+        },
+      };
+      addUtilities(newUtilities, {
+        variants: ["responsive", "hover"],
+      });
+    }),
   ],
-}
+};
